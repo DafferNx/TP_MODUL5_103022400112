a4566d43a4b4fd7ca335607381cc72ce76e66820
DafferNx <filbertdeven20@gmail.com>
2026-03-28 17:54:02 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400112/Program.cs
+++ TP_MODUL5_103022400112/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+    
+}
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric haloGen = new HaloGeneric();
+        haloGen.SapaUser<string>("Filbert");
+
+        DataGeneric<string> dataGen = new DataGeneric<string>("103022400112");
+        dataGen.printData();
+    }
+}
+
+class DataGeneric<T>
+{
+    public T Data { get; set; }
+    
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + Data);
+    }
+}
